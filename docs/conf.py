79bccfbaaa7916971b652d686b1af58336720538
Kevinnota <kevin_nota@eva.mpg.de>
2025-07-30 16:14:06 +0200

    update

--- docs/conf.py
+++ docs/conf.py
@@ -10,22 +10,38 @@ project = 'NED-flow'
 copyright = '2025, Kevin Nota, Benjamin Vernot'
 author = 'Kevin Nota, Benjamin Vernot'
 release = 'stable-beta_1'
+language = 'en'
 
 # -- General configuration ---------------------------------------------------
 
+
 extensions = [
+    'sphinx.ext.intersphinx',
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx_rtd_dark_mode',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
+source_suffix = '.rst'
 exclude_patterns = []
+locale_dirs = ['locale/']
+gettext_compact = False
+
+master_doc = 'index'
+suppress_warnings = ['image.nonlocal_uri']
+pygments_style = 'default'
 
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'logo_only': True,
+    'navigation_depth': 5,
+}
+html_context = {}
 
 # -- Dark mode toggle --------------------------------------------------------
 
