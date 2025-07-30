e12a3c809b7d6ffd8e2b0f4810e61614b461dc19
Kevinnota <kevin_nota@eva.mpg.de>
2025-07-30 16:05:35 +0200

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,29 +1,36 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'NED-flow'
 copyright = '2025, Kevin Nota, Benjamin Vernot'
 author = 'Kevin Nota, Benjamin Vernot'
-release = '0.1-beta'
+release = 'stable-beta_1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_dark_mode',
+]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
+# -- Dark mode toggle --------------------------------------------------------
+
+default_dark_mode = True
+
+# -- Static files (custom CSS, images, etc.) ---------------------------------
+
+html_static_path = ['_static']
