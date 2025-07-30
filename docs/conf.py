6a44e16dc080f8e952bc46c25e0f08674b530d69
Kevinnota <kevin_nota@eva.mpg.de>
2025-07-30 15:44:53 +0200

    update

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import sphinx_rtd_theme
-
 project = 'NED-flow'
 copyright = '2025, Kevin Nota, Benjamin Vernot'
 author = 'Kevin Nota, Benjamin Vernot'
@@ -26,7 +24,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
